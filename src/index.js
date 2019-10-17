e44bb84b9bdf681776836024380756c69404d109
Christopher Ksiazak <cksiazak@gmail.com>
2019-10-16 21:57:37 -0600

    added dependencies, built out beginning redux store and reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './store/reducers';
+import thunk from 'redux-thunk';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Redux store initialization
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
---
+++ src/store/reducers/placeholderReducer.js
@@ -0,0 +1,7 @@
+// Here is a placeholder reducer to get rid of any beginning errors.
+export const placeholderReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};

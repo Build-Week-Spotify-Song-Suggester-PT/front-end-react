4e4e13ad10f5c129cdf50d0cbacda39d3bf9cfaf
Amy Barba <Amybarba70@gmail.com>
2019-10-17 19:52:51 -0700

    structure code setup for Login Component

--- src/Components/User/Login.js
+++ src/Components/User/Login.js
@@ -1,5 +1,67 @@
 import React from "react"; 
+// import token authentication
+import { axiosWithAuth } from '../auth/axiosWithAuth';
 
-export default (params) => {
-    
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: "",
+            password: ""
+        }
+    };
+
+handleChange = e => {
+    this.setState({
+        credentials: {
+            ...this.state.credentials, 
+            [e.target.name]: e.target.value
+        }
+    });
+};
+
+login = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+    // make a post request to retrieve a token from the api
+    .post("/login", this.state.credentials)
+    .then(response => {
+        localStorage.setItem("token", response.data.payload);
+        // once the token is handled, navigate to 
+        this.props.history.push
+        // ("") place target component within parentheses
+    })
+    .catch(error => console.log(error));
+};
+
+render() {
+    return (
+        <>
+        <form onSubmit={this.login}>
+            <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+            />
+            <input 
+            type="password"
+            name="password"
+            placeholder="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+            />
+            <button>Log In</button>
+        </form>
+        </>
+    );
 }
+}
+
+export default Login;
+
+
+// export default (params) => {
+    
+// }

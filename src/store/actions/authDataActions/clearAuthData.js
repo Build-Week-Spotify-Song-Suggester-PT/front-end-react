260231b3ebb09d750f0515a3121d47615d6afa12
Christopher Ksiazak <cksiazak@gmail.com>
2019-10-22 21:08:17 -0600

    minor change to action

--- src/store/actions/authDataActions/clearAuthData.js
+++ src/store/actions/authDataActions/clearAuthData.js
@@ -1,4 +1,4 @@
-export const CLEAR_AUTH_DATA = 'CLEAR_FORM_DATA';
+export const CLEAR_AUTH_DATA = 'CLEAR_AUTH_DATA';
 
 export const clearAuthData = () => dispatch => {
   dispatch({ type: CLEAR_AUTH_DATA });
